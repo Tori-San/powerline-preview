5e7e1d7672a86955d5fa83ca3d7d9c6cc14c9aa3
Kai Vogelgesang <s8kavoge@stud.uni-saarland.de>
2018-05-05 17:10:28 +0200

    Re-Add most functionality, Add terminal colors and clipboard/file output

--- preview.js
+++ preview.js
@@ -5,6 +5,20 @@
  *   or "sep" classes appear last in the className
  */
 
+/* Set appropriate text color
+ *
+ * Example: <div class="term-foobar"> gets
+ *          color: var(--t-foobar)
+ */
+function setTermClassStyle() {
+    $("[class*=term]").each(function() {
+        let cls = this.className;
+        cls = "t-" + cls.slice(cls.indexOf("term") + 5);
+        this.style.color = "var(--"+cls+")";
+        this.style.marginRight = "5px";
+    });
+}
+
 /* Set appropriate text and background color
  *
  * Example: <div class="level-foobar"> gets
@@ -57,6 +71,73 @@ function redrawSeperators() {
     });
 }
 
+var nameMap = {
+    "--l1-fg": "level1_fg",
+    "--l1-bg": "level1_bg",
+    "--l1a-fg": "level1_fg_alt",
+    "--l1a-bg": "level1_bg_alt",
+    "--l1v-fg": "level1_fg_vis",
+    "--l1v-bg": "level1_bg_vis",
+    "--l1r-fg": "level1_fg_rep",
+    "--l1r-bg": "level1_bg_rep",
+    "--l2-fg": "level2_fg",
+    "--l2-bg": "level2_bg",
+    "--l2a-fg": "level2_fg_alt",
+    "--l2a-bg": "level2_bg_alt",
+    "--l3-fg": "level3_fg",
+    "--l3-bg": "level3_bg",
+    "--l3a-fg": "level3_fg_alt",
+    "--l3a-bg": "level3_bg_alt",
+    "--l4-fg": "level4_fg",
+    "--l4-bg": "level4_bg",
+    "--l4a-fg": "level4_fg_alt",
+    "--l4a-bg": "level4_bg_alt",
+    "--t-background": "background",
+    "--t-foreground": "foreground",
+    "--t-black": "black",
+    "--t-altblack": "alt_black",
+    "--t-red": "red",
+    "--t-altred": "alt_red",
+    "--t-green": "green",
+    "--t-altgreen": "alt_green",
+    "--t-yellow": "yellow",
+    "--t-altyellow": "alt_yellow",
+    "--t-blue": "blue",
+    "--t-altblue": "alt_blue",
+    "--t-magenta": "magenta",
+    "--t-altmagenta": "alt_magenta",
+    "--t-cyan": "cyan",
+    "--t-altcyan": "alt_cyan",
+    "--t-white": "white",
+    "--t-altwhite": "alt_white",
+}
+
+/* Update stuff that is not automatically updated by css variables */
+function updatePreview() {
+    redrawSeperators();
+    s = "";
+    for (let k in nameMap) {
+        s += nameMap[k] + ": " + $("body").css(k) + "\n";
+    }
+    $("#output").html(s);
+    $("#output-file").attr("href", "data:text/plain;charset=utf-8," + encodeURIComponent(s));
+}
+
+$("#output-clipboard").click(function() {
+    $("#output").show();
+    $("#output").select();
+    document.execCommand("copy");
+    $("#output").hide();
+});
+
+/* Make buttons apply the currently selected color */
+$("[id^=button]").click(function() {
+    let cls = this.id;
+    cls = "--" + cls.slice(cls.indexOf("button") + 7);
+    $("body").css(cls, $("#foobar").css("backgroundColor"))
+    updatePreview();
+});
+
 /* Draw uploaded image onto the canvas */
 function imgUploadHandler(e) {
     var reader = new FileReader();
@@ -85,9 +166,20 @@ $("#canvas-placeholder-text").click(function() {
     $("#img-upload").click();
 });
 
+/* Color selection
+ */
+$("#cs").click(function(e) {
+    let offset = $(this).offset()
+    let rgba = this.getContext('2d')
+        .getImageData(e.pageX - offset.left, e.pageY - offset.top, 1, 1)
+        .data;
+    $("#foobar").css("backgroundColor", chroma.rgb(rgba).css());
+});
+
 /* Initialize page:
  *
  * Assign levels and update seperators
  */
 setLevelClassStyle();
-redrawSeperators();
+setTermClassStyle();
+updatePreview();
